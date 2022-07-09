16da98312b7c1f72e617a3ac9b7de4c897a54d70
Isaac Bythewood <isaac@bythewood.me>
2022-07-09 00:23:27 +0000

    Fix error if no bookmarks

--- newtab.js
+++ newtab.js
@@ -7,14 +7,18 @@ const currentBookmarksElement = document.getElementById('currentBookmarks');
 const faviconFolder = '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="white" viewBox="0 0 16 16"><path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/></svg>';
 
 const getBookmarks = () => {
-  return chrome.bookmarks.getTree().then(bookmarks => {
-    const children = bookmarks[0].children;
-    for (let i = 0; i < children.length; i++) {
-      if (children[i].title === 'Bookmarks bar') {
-        return children[i].children;
+  try {
+    return chrome.bookmarks.getTree().then(bookmarks => {
+      const children = bookmarks[0].children;
+      for (let i = 0; i < children.length; i++) {
+        if (children[i].title === 'Bookmarks bar') {
+          return children[i].children;
+        }
       }
-    }
-  });
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 const createBookmarksList = (bookmarks) => {
@@ -51,11 +55,15 @@ const createBookmarksList = (bookmarks) => {
 }
 
 const updateCurrentBookmarks = () => {
-  getBookmarks().then(bookmarks => {
-    currentBookmarksElement.innerHTML = '';
-    const bookmarksList = createBookmarksList(bookmarks);
-    currentBookmarksElement.appendChild(bookmarksList);
-  });
+  try {
+    getBookmarks().then(bookmarks => {
+      currentBookmarksElement.innerHTML = '';
+      const bookmarksList = createBookmarksList(bookmarks);
+      currentBookmarksElement.appendChild(bookmarksList);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 updateCurrentBookmarks();
